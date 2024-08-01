7e35923a7982abc37e2f51c188e9dbf0121ac065
katieemelianova <katie_emelianova@hotmail.com>
2024-08-01 08:59:27 +0200

    Delete run_emboss_skipredundant.sh

--- run_emboss_skipredundant.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#SBATCH --partition=short
-#SBATCH --cpus-per-task=8
-#SBATCH --mem=16G
-
-
-skipredundant -sequences Hsand_mRNA.fasta -threshold 90 -outseq emboss_out -gapextend 0.5 -gapopen 10 -mode 1 -redundantoutseq redundant_hsand
